e0507debdab41e8483b73356941b9ac94245ed72
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-05 06:46:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,6 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator (johhny)
-  return ["Hello, my name is Johnny."]
-end
 
 def batch_badge_creator(attendees)
   attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
