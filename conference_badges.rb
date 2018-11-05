921e44aa00127e9ae4dee8f4e0785c9083990a1e
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-05 06:53:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,10 +4,21 @@ end
 
 
 def batch_badge_creator(attendees)
-  attendees.collect
-  return (badges)
+  attendees.collect do |name|
+    badge_maker(name)
+  end
+ end
+ def assign_rooms(attendees)
+  room=0
+  attendees.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
 end
-
-def batch_badge_creator (johhny)
-  return ["Hello, my name is Johnny."]
-end
+ def printer(attendees)
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
