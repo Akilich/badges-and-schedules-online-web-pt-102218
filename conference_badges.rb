f15fe617f6053c4a515844b3453d73809f45d43c
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-05 06:26:19 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  name = "Arel"
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  badges = "Hello, my name is Edsger.",
+                  "Hello, my name is Ada.",
+                  "Hello, my name is Charles.",
+                  "Hello, my name is Alan.",
+                  "Hello, my name is Grace.",
+                  "Hello, my name is Linus.",
+                  "Hello, my name is Matz."
+  return (badges)
+end
